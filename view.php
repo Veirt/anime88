443279968270ed5b0934327667c70152ac7ce6fd
IkkiCantCode <ikkiyukkigo@gmail.com>
2023-11-05 23:04:02 +0800

    add view

---
+++ view.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php $title = "Anime88 - View - Anime" ?>
+<?php include("includes/head.php") ?>
+
+    <body>
+        <?php include("includes/navbar.php") ?>
+
+        <main>
+            <section class="preview-anime">
+                <div class="poster-anime">
+                    <img src="assets/100kanojo.jpg" alt="#"> 
+                </div>
+
+                <div class="anime-content">
+                    <div class="anime-desc">
+                        <h1> Kimi no Koto ga Daidaidaidaidaisuki na 100-nin no Kanojo </h1>
+                        <p> 
+                            Upon graduating middle school, Rentarou Aijou manages to confess to the girl he loves. Unfortunately, he gets rejected, making it his 100th rejection in a row. Having experienced heartbreak after heartbreak, he goes to a matchmaking shrine and prays with the hope of finally getting a girlfriend in high school. Suddenly, the god of the shrine appears, promising Rentarou that he will meet one hundred soulmates in high school.
+                            Although skeptical at first, Rentarou quickly acknowledges the truth behind the god's words when two of his soulmates—Hakari Hanazono and Karane Inda—confess to him the very same day that they meet him. However, there was one detail that the god forgot to tell Rentarou: if any of his soulmates fails to get into a relationship with him, they will die. Now trapped in a matter of life and death, Rentarou decides to date all of his soulmates.
+                            With a heart so big that it can be shared among one hundred girlfriends, Rentarou makes the most out of his unanticipated high school life, with the Rentarou family growing ever larger!
+                        </p>
+                    </div>
+                    <div class="anime-rank">
+                        <h3>Score</h3>
+                        <h1>7.25</h1>
+                    </div>
+                </div>
+            </section>
+
+            <section class="anime-info">
+                    <div class="genre-anime">
+                        <h1>Genre : </h1>
+                    </div>
+
+                    <div class="more-info">
+                        <h1>Episodes :</h1>
+                        <p>Episode Number</p>
+
+                        <h1>Status :</h1>
+                        <p>Anime Status</p>
+
+                        <h1>Season :</h1>
+                        <p>Anime Season</p>
+
+                        <h1>Year Released :</h1>
+                        <p>Year Anime Released</p>
+
+                        <h1>Studio : </h1>
+                        <p>Anime Studio</p>
+
+                    </div>
+            </section>
+
+        </main>
+
+    </body>
+
+
+</html>
