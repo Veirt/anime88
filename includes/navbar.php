936c68721edc6c1f0dcfac754ec2792ba34cdaab
Veirt <vveirt@gmail.com>
2023-11-05 19:17:09 +0800

    feat: make navbar logo and name clickable

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,12 @@
 <nav>
 	<div class="logo-section">
-		<img class="logo" src="assets/logo.png" alt="">
-		<h1>Anime88</h1>
+		<a href="index.php">
+			<img class="logo" src="assets/logo.png" alt="Logo">
+
+		</a>
+		<a class="website-name" href="index.php">
+			<h1>Anime88</h1>
+		</a>
 		<button id="toggle-dark">toggle</button>
 	</div>
 </nav>
