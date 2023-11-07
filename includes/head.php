1008b34ceb0b0e328f301a3a6ca1e9098b1d5b92
Veirt <vveirt@gmail.com>
2023-11-07 22:33:50 +0800

    fix: fouc

--- includes/head.php
+++ includes/head.php
@@ -1,16 +1,23 @@
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+    <link rel="stylesheet" type="text/css" href="assets/style.css">
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script defer async src="assets/js/script.js"></script>
+
     <style>
-        html {
-            visibility: visible;
-            opacity: 1;
+        .no-fouc {
+            display: none;
         }
     </style>
 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript">
+        document.documentElement.className = 'no-fouc';
+        $(window).on("load", function() {
+            $('.no-fouc').removeClass('no-fouc');
+        });
+    </script>
 
-    <title><?= $title ?></title>
-
-    <link rel="stylesheet" type="text/css" href="assets/style.css">
-    <script defer async src="assets/js/script.js"></script>
 </head>
