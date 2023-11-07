d45de007d986f032df03756f329d639d61fe758b
IkkiCantCode <ikkiyukkigo@gmail.com>
2023-11-07 23:28:36 +0800

    feat: genre page

---
+++ genre.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php $title = "Anime88 - Genre - Anime" ?>
+<?php include("includes/head.php") ?>
+
+    <body>
+        <?php include("includes/navbar.php") ?>
+
+        <main class="genre-main">
+            <section class="genre-page">
+                <div class="genre-title">
+                    <h1>"Genre" Anime</h1>
+                </div>
+            </section>
+        </main>
+    </body>
+</html>
