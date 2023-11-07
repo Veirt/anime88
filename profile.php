541209e455b66e864200cc2d682f5c2b0c147af9
IkkiCantCode <ikkiyukkigo@gmail.com>
2023-11-07 17:23:28 +0800

    feat: profile page

---
+++ profile.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php $title = "Anime88 - Profile" ?>
+<?php include("includes/head.php") ?>
+
+    <body>
+        <?php include("includes/navbar.php") ?>
+
+        <main>
+            <section class="welcome-user">
+                <h1>HAI!!! Welcome Back, Username</h1>
+            </section>
+        </main>
+    </body>
+</html>
