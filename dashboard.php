aad7fa9a5fd7d1c35758e9c74bd74a7b2179e418
IkkiCantCode <ikkiyukkigo@gmail.com>
2023-11-09 21:56:38 +0800

    feat: dasboard page

---
+++ dashboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php $title = "Anime88 - Dashboard" ?>
+<?php include("includes/head.php") ?>
+
+    <body>
+        <?php include("includes/navbar.php") ?>
+
+        <main>
+            
+        </main>
+    </body>
+</html>
