47c900954e4add05687829826d5253332f05c3fc
Veirt <vveirt@gmail.com>
2023-11-12 18:17:01 +0800

    fix: session hasn't started

--- profile.php
+++ profile.php
@@ -1,43 +1,48 @@
+<?php require("session_start.php") ?>
+<?php require("utils.php") ?>
+
+<!-- TODO: bagusin lagi -->
 <!DOCTYPE html>
 <html lang="en">
 
 <?php $title = "Anime88 - Profile" ?>
 <?php include("includes/head.php") ?>
 
-    <body>
-        <?php include("includes/navbar.php") ?>
+<body>
+    <?php include("includes/navbar.php") ?>
 
-        <main>
-            <section class="welcome-user">
-                <h1>Username</h1>
-            </section>
-            <section>
-                <div class="list-card">
-                    <div class="list-title">
-                        <h1>All Rated Anime</h1>
-                    </div>
-                    <div class="total-rated">
-                        <p>Total Anime Rated : </p>
-                        <p>Mean Score : </p>
-                    </div>
-                        <tbody>
-                            <tr class="table-header">
-                                <th class="table-list number">#</th>
-                                <th class="table-list image">Anime Poster</th>
-                                <th class="table-list title">Anime Title</th>
-                                <th class="table-list score">My Score</th>
-                            </tr>
-                        </tbody>
-                        <tbody>
-                            <tr class="list-data"></tr>
-                                <td class="list-data number"></td>
-                                <td class="list-data image"></td>
-                                <td class="list-data title"></td>
-                                <td class="list-data score"></td>
-                        </tbody>
-
+    <main>
+        <section class="welcome-user">
+            <h1>Username</h1>
+        </section>
+        <section>
+            <div class="list-card">
+                <div class="list-title">
+                    <h1>All Rated Anime</h1>
                 </div>
-            </section>
-        </main>
-    </body>
+                <div class="total-rated">
+                    <p>Total Anime Rated : </p>
+                    <p>Mean Score : </p>
+                </div>
+                <tbody>
+                    <tr class="table-header">
+                        <th class="table-list number">#</th>
+                        <th class="table-list image">Anime Poster</th>
+                        <th class="table-list title">Anime Title</th>
+                        <th class="table-list score">My Score</th>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr class="list-data"></tr>
+                    <td class="list-data number"></td>
+                    <td class="list-data image"></td>
+                    <td class="list-data title"></td>
+                    <td class="list-data score"></td>
+                </tbody>
+
+            </div>
+        </section>
+    </main>
+</body>
+
 </html>
