6add8049ba3fa77384722cb56d412be1a6a874a7
IkkiCantCode <ikkiyukkigo@gmail.com>
2023-11-13 18:51:00 +0800

    modify: profile

--- profile.php
+++ profile.php
@@ -15,32 +15,30 @@
         <section class="welcome-user">
             <h1>Username</h1>
         </section>
-        <section>
-            <div class="list-card">
-                <div class="list-title">
-                    <h1>All Rated Anime</h1>
-                </div>
-                <div class="total-rated">
-                    <p>Total Anime Rated : </p>
-                    <p>Mean Score : </p>
-                </div>
-                <tbody>
+        <section class="list-card">
+            <h1 class="list-title">All Rated Anime</h1>
+            <div class="total-rated">
+                <p>Total Anime Rated : </p>
+                <p>Mean Score : </p>
+            </div>
+            <table class="table-list">
+                <thead>
                     <tr class="table-header">
-                        <th class="table-list number">#</th>
-                        <th class="table-list image">Anime Poster</th>
-                        <th class="table-list title">Anime Title</th>
-                        <th class="table-list score">My Score</th>
+                        <th class="table-number">#</th>
+                        <th class="table-image">Anime Poster</th>
+                        <th class="-table-title">Anime Title</th>
+                        <th class="table-score">My Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="list-data">
+                        <td class="table-number">1</td>
+                        <td class="table-image"><img src="assets\poster\vinland-saga.png" alt=""></td>
+                        <td class="table-title">Vinland Saga</td>
+                        <td class="table-score">10</td>
                     </tr>
                 </tbody>
-                <tbody>
-                    <tr class="list-data"></tr>
-                    <td class="list-data number"></td>
-                    <td class="list-data image"></td>
-                    <td class="list-data title"></td>
-                    <td class="list-data score"></td>
-                </tbody>
-
-            </div>
+            </table>
         </section>
     </main>
 </body>
